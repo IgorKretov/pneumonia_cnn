cacad74a17912be905f41a03a62315a85c7cf4b7
Samuel <samuelmwkim@gmail.com>
2021-01-13 23:08:41 +0900

    Added view pages

--- frontend/views.py
+++ frontend/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'frontend/index.html')
+
+def register(request):
+    return render(request, 'frontend/register.html')
+
+def login(request):
+    return render(request, 'frontend/login.html')
